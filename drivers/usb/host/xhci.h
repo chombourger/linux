1582103e48ec5fee899a64c4baf1d78c578cc96f
Sandeep Singh <sandeep.singh@amd.com>
2020-10-28 22:31:23 +0200

    usb: xhci: Workaround for S3 issue on AMD SNPS 3.0 xHC
    commit 2a632815683d2d34df52b701a36fe5ac6654e719 upstream.
    
    On some platform of AMD, S3 fails with HCE and SRE errors. To fix this,
    need to disable a bit which is enable in sparse controller.
    
    Cc: stable@vger.kernel.org #v4.19+
    Signed-off-by: Sanket Goswami <Sanket.Goswami@amd.com>
    Signed-off-by: Sandeep Singh <sandeep.singh@amd.com>
    Signed-off-by: Mathias Nyman <mathias.nyman@linux.intel.com>
    Link: https://lore.kernel.org/r/20201028203124.375344-3-mathias.nyman@linux.intel.com
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Signed-off-by: Cedric Hombouger <cedric_hombourger@mentor.com>

--- drivers/usb/host/xhci.h
+++ drivers/usb/host/xhci.h
@@ -1876,6 +1876,7 @@ struct xhci_hcd {
 #define XHCI_REVERSE_IN_OUT     BIT_ULL(36)
 #define XHCI_STOP_TRANSFER_IN_BLOCK	BIT_ULL(37)
 #define XHCI_STOP_EP_IN_U1	BIT_ULL(38)
+#define XHCI_DISABLE_SPARSE	BIT_ULL(39)
 
 	unsigned int		num_active_eps;
 	unsigned int		limit_active_eps;
